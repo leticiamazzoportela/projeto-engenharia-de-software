dbe89580d186b29ed10e658cb705b7db036c5e81
Igor Neves Faustino <igornfaustino@gmail.com>
2018-10-25 15:06:34 -0300

    add Connection exception

--- src/main.py
+++ src/main.py
@@ -2,18 +2,20 @@ import csv_util
 import florabrasil_api
 import plantlist_crawler
 
+print("\n\n----------------------------")
+print("processando lista de entrada.....")
+csv_data = csv_util.readInput()
+
 try:
-	print("\n\n----------------------------")
-	print("processando lista de entrada.....")
-	csv_data = csv_util.readInput()
 	print("Verificando nomes.....")
 	print("\nanalisando flora do brasil....\n")
 	florabrasil_data = florabrasil_api.getNames(csv_data)
+
+	print("\nanalisando plantlist....\n")
+	plantlist_data = plantlist_crawler.getNames(florabrasil_data)
 except ConnectionError:
 	print("Falha na rede...")
 	exit(1)
-print("\nanalisando plantlist....\n")
-plantlist_data = plantlist_crawler.getNames(florabrasil_data)
 
 print("Salvando CSV")
-csv_util.saveCSV(plantlist_data)
+csv_util.saveSheet(plantlist_data)
