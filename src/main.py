8ab3a78ece835825c096dc9bb15b305c67da43e6
Igor Neves Faustino <igornfaustino@gmail.com>
2018-11-01 14:16:50 -0300

    update specieslink crawler

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import csv_util
 import florabrasil_api
 import plantlist_crawler
+import specieslink_crawler
 import util
 import json
 
@@ -8,21 +9,32 @@ print("\n\n----------------------------")
 print("processando lista de entrada.....")
 csv_data = csv_util.readInput()
 
-try:
-	print("Verificando nomes.....")
-	print("\nanalisando flora do brasil....\n")
-	florabrasil_data = florabrasil_api.getNames(csv_data)
+# try:
+# 	print("Verificando nomes.....")
+# 	print("\nanalisando flora do brasil....\n")
+# 	florabrasil_data = florabrasil_api.getNames(csv_data)
 
-	print("\nanalisando plantlist....\n")
-	plantlist_data = plantlist_crawler.getNames(florabrasil_data)
-except ConnectionError:
-	print("Falha na rede...")
-	exit(1)
+# 	print("\nanalisando plantlist....\n")
+# 	plantlist_data = plantlist_crawler.getNames(florabrasil_data)
+# except ConnectionError:
+# 	print("Falha na rede...")
+# 	exit(1)
 
-print("Salvando CSV")
-csv_util.saveSheet(plantlist_data)
+# with open('plant_flora.json', 'w') as outfile:
+#     json.dump(plantlist_data, outfile)
+
+plantlist_data = json.loads(open("plant_flora.json").read())
+
+# print("Salvando CSV")
+# csv_util.saveSheet(plantlist_data)
 
 valid_names = util.get_list_of_valid_names(plantlist_data)
 
-with open('validname.json', 'w') as outfile:
+with open('src/data/validname.json', 'w') as outfile:
     json.dump(valid_names, outfile)
+
+
+specieslink =specieslink_crawler.get_data(valid_names)
+
+with open('src/data/specieslink.json', 'w') as outfile:
+    json.dump(specieslink, outfile)
--- src/util.py
+++ src/util.py
@@ -6,19 +6,26 @@ def normalize(_str):
         return unidecode.unidecode(_str)
     return ""
 
+def normalize_str(_str):
+    return _str.strip().lower()
 
 def get_list_of_valid_names(species):
     species_list = []
     for specie in species:
-        if (specie["status_florabrasil"] != "nao_encontrado"):
+        if (specie["status_florabrasil"] != "nao_encontrado" and specie.__contains__("florabrasil")):
             species_list.append(specie["florabrasil"])
-        elif (specie["status_plantlist"] != "nao_encontrado"):
+        elif (specie["status_plantlist"] != "nao_encontrado"and specie.__contains__("plantlist")):
             species_list.append(specie["plantlist"])
 
     return species_list
 
+def remove_author(specie):
+    specie_token = specie.split(" ")
+    return specie_token[0] + " " + specie_token[1]
 
 if __name__ == "__main__":
+    import json
+
     print(get_list_of_valid_names([
         {
             "nome": "Hygrophila guianensis Nees",
