4ec011e9af249d71e1724bc5b4b00b5de4b77824
claudiaps <clp.sampedro@gmail.com>
2018-12-05 11:05:10 -0200

    starting country validation

---
+++ src/triagem.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+import requests
+import json
+import os
+import io
+import util
+
+countrys = ['argentina', 'bolívia', 'brasil','chile', 'colômbia', 'equador',  'guiana', 'paraguai', 'peru', 'suriname', 'uruguai', 'venezuela', 'bolivia', 'brazil', 'colombia', 'ecuador', 'paraguay', 'uruguay']
+
