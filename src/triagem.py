ae669dcc1c2144adcbe26d10f6771537a48aea06
claudiaps <clp.sampedro@gmail.com>
2018-12-05 20:30:47 -0200

    finish validation

--- src/triagem.py
+++ src/triagem.py
@@ -31,22 +31,26 @@ def read_gbif():
 
 def read_spicieslink():
     new_data = {}
-        data = []
-        with open('./data/spicieslink.json') as json_gbif:
-            data = json.load(json_gbif)
-            for plant in data:
-                oc = []
-                for ocurrence in data[plant]:
-                    if(ocurrence['country'].lower() in countrys):
+    data = []
+    with open('./data/specieslink.json') as json_specieslink:
+        data = json.load(json_specieslink)
+        for plant in data:
+            oc = []
+            for ocurrence in data[plant]:
+                if(ocurrence['pais'].lower() in countrys):
+                    oc.append(ocurrence)
+                elif(ocurrence['lat'] != "" and ocurrence['long'] != ""):
+                    if((float(ocurrence['lat'])<= lat[0] and float(ocurrence['lat']) >= lat[1]) and (float(ocurrence['long']) <= longt[0] and float(ocurrence['long']) >= longt[1])):
                         oc.append(ocurrence)
-                    elif(ocurrence['decimalLatitude'] != "" and ocurrence['decimalLongitude'] != ""):
-                        if((float(ocurrence['decimalLatitude'])<= lat[0] and float(ocurrence['decimalLatitude']) >= lat[1]) and (float(ocurrence['decimalLongitude']) <= longt[0] and float(ocurrence['decimalLongitude']) >= longt[1])):
-                            oc.append(ocurrence)
-                    else:
-                        print('não esta na ame. do sul')
-                new_data[plant] = oc
-        return new_data
+                else:
+                    print('não esta na ame. do sul')
+            new_data[plant] = oc
+    return new_data
 
-new_data = read_gbif()
-with open('./data/saida_gbif_validado.json', 'w') as data_file:
-    data = json.dump(new_data, data_file)
+# gbif_data = read_gbif()
+# with open('./data/saida_gbif_validado.json', 'w') as data_file:
+#     data = json.dump(gbif_data, data_file)
+
+specieslink_data = read_spicieslink()
+with open('./data/specieslink_validade', 'w') as data_file:
+    data = json.dump(specieslink_data, data_file)
